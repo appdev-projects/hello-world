8b109d543a729d792191d8f187986b5695c954e4
Ben Purinton <ben.purinton@gmail.com>
2023-06-12 09:20:08 -0800

    update specs

---
+++ spec/features/hello_world_spec.rb
@@ -0,0 +1,16 @@
+describe "The home page" do
+  it "renders an HTML page", points: 1 do
+    visit "/"
+
+    expect(page.status_code).to be(200)
+  end
+end
+
+describe "The home page" do
+  it "has a heading with the text 'Hello, world!'", points: 1, hint: h("copy_must_match label_for_input") do
+    visit "/"
+
+    expect { find("h1", :text => /Hello, world!/i) }.to_not raise_error,
+      "Expected page to have an <h1> with the text 'Hello, world!', but didn't find one."
+  end
+end
--- spec/features/sample_spec.rb
+++
@@ -1,6 +0,0 @@
-describe "This project" do
-  it "has no tests" do
-    expect(1).to eq(1)
-  end
-end
-
