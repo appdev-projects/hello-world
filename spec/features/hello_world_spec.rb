c0b83210ec84cb719e776301903f62a9bae6ec67
Ben Purinton <ben.purinton@gmail.com>
2023-07-17 21:52:01 +0000

    update spec whitespace matcher

--- spec/features/hello_world_spec.rb
+++ spec/features/hello_world_spec.rb
@@ -10,7 +10,7 @@ describe "The home page" do
   it "has a heading with the text 'Hello, world!'", points: 1, hint: h("copy_must_match") do
     visit "/"
 
-    expect { find("h1", :text => /Hello, world!/i) }.to_not raise_error,
+    expect { find("h1", :text => /Hello,\s+world!/i) }.to_not raise_error,
       "Expected page to have an h1 element with the text 'Hello, world!', but didn't find one."
   end
 end
